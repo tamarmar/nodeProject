711a1f77373656524b023e05f6c4eac625732eb2
Yaelkofman <117908403+Yaelkofman@users.noreply.github.com>
2023-06-21 12:54:23 +0300

    after fix!!

--- UserRouter.js
+++ UserRouter.js
@@ -4,7 +4,7 @@ const UserController = require("./UserController.js");
 UserRouter.get("/", UserController.getAllUsers);
 UserRouter.get("/:id", UserController.getUserById);
 UserRouter.post("/", UserController.addUser);
-UserRouter.put("/:password", UserController.updateUser);
-UserRouter.delete("/:password", UserController.deleteUser); 
+UserRouter.put("/:id", UserController.updateUser);
+UserRouter.delete("/:id", UserController.deleteUser); 
 
 module.exports = UserRouter;
